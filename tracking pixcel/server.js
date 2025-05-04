decc3f40ecb753a71ee5c397160b678e3e5bd2c7
shubhammagar <magarshubham13@gmail.com>
2025-05-04 13:47:54 +0530

    added process port

--- tracking pixcel/server.js
+++ tracking pixcel/server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const path = require('path');
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // In-memory store for tracking events
 const trackingEvents = [];
